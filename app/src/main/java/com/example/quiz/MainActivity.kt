7be6e128bfc3478033916aaf5f92b1bd0a8d1d44
JangWoojun <woojun0107@naver.com>
2022-10-31 17:46:04 +0900

    2022/10/31 이름 칸이 비어 있으면 Toast 메세지를 띄우고 채워져 있다면 액티비티 이동시키는 기능 추가

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<Button>(R.id.startBtn)
+        val nameTv = findViewById<EditText>(R.id.nameTv)
+
+        startBtn.setOnClickListener {
+            if (nameTv.text != null){
+                val intent = Intent(this,QnaActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else {
+                Toast.makeText(this,"Please enter your name",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
