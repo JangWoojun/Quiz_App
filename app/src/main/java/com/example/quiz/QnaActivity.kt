7a4e3331d971d7855b5d7ffe834b8a875a2bcd5a
JangWoojun <woojun0107@naver.com>
2022-11-03 21:09:08 +0900

    2022/11/03 Constants 값을 연결한 것을 함수로 리팩토링

--- app/src/main/java/com/example/quiz/QnaActivity.kt
+++ app/src/main/java/com/example/quiz/QnaActivity.kt
@@ -2,11 +2,17 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
 import com.example.quiz.databinding.ActivityQnaBinding
 
 class QnaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityQnaBinding
+
+    private var mCurrentPosition : Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qna)
@@ -14,12 +20,17 @@ class QnaActivity : AppCompatActivity() {
         binding = ActivityQnaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val questionList = Constants.getQuestion() // Constants에서 질문과 대답, 국기 등이 당긴 리스트 받아옴
-        var currentPosition = 1 // 위치를 나타내는 변수
-        val question : Question = questionList[currentPosition-1] // questionList에서 인덱스 번호에 해당하는 것들을 담은 변수 생성
+        mQuestionsList = Constants.getQuestion() // Constants에서 질문과 대답, 국기 등이 당긴 리스트 받아옴
+        setQuestion()
 
-        binding.progressBar.progress = currentPosition // progressBar에 지점을 위치를 나타내는 변수로 지정
-        binding.progressBarText.text = "${currentPosition}/${binding.progressBar.max}" // progressBarText를 위치를 나타내는 변수로 지정
+    }
+
+    private fun setQuestion() {
+        val question: Question = mQuestionsList!![mCurrentPosition - 1] // questionList에서 인덱스 번호에 해당하는 것들을 담은 변수 생성
+
+        binding.progressBar.progress = mCurrentPosition // progressBar에 지점을 위치를 나타내는 변수로 지정
+        binding.progressBarText.text =
+            "${mCurrentPosition}/${binding.progressBar.max}" // progressBarText를 위치를 나타내는 변수로 지정
         binding.tvQuestion.text = question.question // 질문을 question에 질문으로 지정
 
         binding.tvImage.setImageResource(question.image) // 이미지를 question에 이미지로 지정
