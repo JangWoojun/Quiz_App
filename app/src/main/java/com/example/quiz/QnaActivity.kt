273b74bcac5d6cfa377aa5b5d471e5d362be32e8
JangWoojun <woojun0107@naver.com>
2022-11-03 15:47:55 +0900

    2022/11/03 viewBinding 추가

--- app/src/main/java/com/example/quiz/QnaActivity.kt
+++ app/src/main/java/com/example/quiz/QnaActivity.kt
@@ -2,12 +2,17 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.quiz.databinding.ActivityQnaBinding
 
 class QnaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityQnaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qna)
-        val questionsList = Constants.getQuestion()
+
+        binding = ActivityQnaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
