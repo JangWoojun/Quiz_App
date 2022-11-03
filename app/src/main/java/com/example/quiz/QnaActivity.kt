b8fd03e6ced0d0849234ef5e818b94acbd6d6066
JangWoojun <woojun0107@naver.com>
2022-11-03 21:53:27 +0900

    2022/11/03 주석 추가

--- app/src/main/java/com/example/quiz/QnaActivity.kt
+++ app/src/main/java/com/example/quiz/QnaActivity.kt
@@ -28,6 +28,7 @@ class QnaActivity : AppCompatActivity(),OnClickListener {
         mQuestionsList = Constants.getQuestion() // Constants에서 질문과 대답, 국기 등이 당긴 리스트 받아옴
         setQuestion()
 
+        // 해당 버튼들을 클릭 시 밑에서 작성한 onClick 실행
         binding.tvOptionOne.setOnClickListener(this)
         binding.tvOptionTwo.setOnClickListener(this)
         binding.tvOptionThree.setOnClickListener(this)
@@ -54,6 +55,8 @@ class QnaActivity : AppCompatActivity(),OnClickListener {
         binding.tvOptionThree.text = question.option3
         binding.tvOptionFour.text = question.option4
 
+        // 만약 mCurrentPosition가 질문 리스트 사이즈와 같아진다면 버튼 텍스트를 제출로 변경
+
         if(mCurrentPosition == mQuestionsList!!.size){
             binding.btnSubmit.text = "제출"
         }
@@ -64,6 +67,7 @@ class QnaActivity : AppCompatActivity(),OnClickListener {
     }
 
     private fun defaultOptionsView(){
+        // options이라는 ArrayList 해당하는 인덱스 번호에 자신을 담는다
         val options = ArrayList<TextView>()
         binding.tvOptionOne.let {
             options.add(0,it)
@@ -78,6 +82,8 @@ class QnaActivity : AppCompatActivity(),OnClickListener {
             options.add(3,it)
         }
 
+        // for문으로 options에서 하나씩 꺼내와 기본상태로 초기화 시킴
+
         for (i in options){
             i.setTextColor(Color.parseColor("#7A8089"))
             i.typeface = Typeface.DEFAULT
@@ -89,10 +95,11 @@ class QnaActivity : AppCompatActivity(),OnClickListener {
     }
 
     private fun selectedOptionView(tv:TextView,selectedOptionNum:Int){
+        // 클릭 할 때마다 초기화 시키는 함수 실행
         defaultOptionsView()
-
+        // 정담 체크를 위해 자신이 클릭한 번호를 저장
         mSelectedOptionPosition = selectedOptionNum
-
+        // 해당하는 버튼에 색을 변경
         tv.setTextColor(Color.parseColor("#363A43"))
         tv.setTypeface(tv.typeface,Typeface.BOLD)
         tv.background = ContextCompat.getDrawable(
@@ -102,6 +109,7 @@ class QnaActivity : AppCompatActivity(),OnClickListener {
     }
 
     override fun onClick(p0: View?) {
+        // 클릭 시 해당하는 버튼을 selectedOptionView함수에 넘긴
         when(p0?.id){
             R.id.tv_option_one -> {
                 selectedOptionView(binding.tvOptionOne,1)
