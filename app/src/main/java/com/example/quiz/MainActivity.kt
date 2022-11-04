cb3e8f3e86bcf151feef622f4c750b0038b0ff8d
JangWoojun <woojun0107@naver.com>
2022-11-04 14:42:08 +0900

    2022/11/04 viewBinding으로 변경

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -6,17 +6,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.quiz.databinding.ActivityMainBinding
+import com.example.quiz.databinding.ActivityQnaBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startBtn = findViewById<Button>(R.id.startBtn)
-        val nameTv = findViewById<EditText>(R.id.nameTv)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        startBtn.setOnClickListener {
-            if (nameTv.text.isNotEmpty()){
+        binding.startBtn.setOnClickListener {
+            if (binding.nameTv.text!!.isNotEmpty()){
                 val intent = Intent(this,QnaActivity::class.java)
                 startActivity(intent)
                 finish()
