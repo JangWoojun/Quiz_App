c2ad1f20dd1ab83d8155bfb89a462b82d0784de4
JangWoojun <woojun0107@naver.com>
2022-11-04 14:43:34 +0900

    2022/11/04 viewBinding 추가

---
+++ app/src/main/java/com/example/quiz/ResultActivity.kt
@@ -0,0 +1,18 @@
+package com.example.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quiz.databinding.ActivityQnaBinding
+import com.example.quiz.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+    lateinit var binding: ActivityResultBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
